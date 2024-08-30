787e90df8d3d2c5c4787417379f1e0600174d3ad
Jim Cornmell <jco@snomed.org>
2024-08-30 15:28:31 +0100

    PIP-435 Add tests for /account and /account/logout endpoints.

--- src/test/groovy/org/snomed/ssoservice/rest/AccountControllerTest.groovy
+++ src/test/groovy/org/snomed/ssoservice/rest/AccountControllerTest.groovy
@@ -1,5 +1,6 @@
 package org.snomed.ssoservice.rest
 
+import org.snomed.ssoservice.rest.dto.UserDTO
 import org.snomed.ssoservice.service.CrowdRestClient
 import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
@@ -133,5 +134,90 @@ class AccountControllerTest extends Specification {
             cookie.getDomain() == "ihtsdotools.org"
             cookie.getPath() == "/"
             !cookie.getSecure()
+    }
+
+
+    def "validate /account/logout endpoint should return 200/OK when user is already logged in"() {
+        given: "A real user"
+            def jsonPayload = '{"login": "realuser", "password": "realpassword"}'
+
+        and: "Mocked crowdRestClient response"
+            1 * crowdRestClient.authenticate(_, _) >> "SomeToken"
+
+        when: "Login"
+            def requestBuilderForAuth = post("/api/authenticate")
+                    .content(jsonPayload)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+
+            def responseForAuth = mockMvc.perform(requestBuilderForAuth).andReturn().response
+            def cookie = responseForAuth.getCookie("dev-ims-ihtsdo")
+
+        and:  "Then logout"
+            def requestBuilderForAccountLogout = post("/api/account/logout")
+                    .cookie(cookie)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+
+            def response = mockMvc.perform(requestBuilderForAccountLogout).andReturn().response
+
+        then:
+            responseForAuth.status == HttpStatus.OK.value()
+            cookie.getValue() == ""
+            cookie.getName() == "dev-ims-ihtsdo"
+            cookie.getMaxAge() == 0
+            cookie.getDomain() == "ihtsdotools.org"
+            cookie.getPath() == "/"
+            !cookie.getSecure()
+    }
+
+    def "validate /account/logout endpoint should return 200/OK and cookie is null"() {
+        when: "Logout without auth (should never happen!)"
+            def requestBuilderForAccountLogout = post("/api/account/logout")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+
+            def response = mockMvc.perform(requestBuilderForAccountLogout).andReturn().response
+            def cookie = response.getCookie("dev-ims-ihtsdo")
+
+        then:
+            response.status == HttpStatus.OK.value()
+            cookie == null
+    }
+
+    def "validate /account endpoint should return 200/OK"() {
+        given: "A real user"
+            def jsonPayload = '{"login": "realuser", "password": "realpassword"}'
+            def userDTO = new UserDTO('joe', 'asecret', 'Joe', 'Smith', 'joe@example.com', 'someLangKey', null)
+
+        and: "Mocked crowdRestClient response"
+            1 * crowdRestClient.authenticate(_, _) >> "SomeToken"
+            1 * crowdRestClient.getUserByToken(_) >> userDTO
+
+        when: "Login"
+            def requestBuilderForAuth = post("/api/authenticate")
+                    .content(jsonPayload)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+
+            def responseForAuth = mockMvc.perform(requestBuilderForAuth).andReturn().response
+            def cookie = responseForAuth.getCookie("dev-ims-ihtsdo")
+
+        and: "Then get account info"
+            def requestBuilderForAccount = get("/api/account")
+                    .cookie(cookie)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+
+            def response = mockMvc.perform(requestBuilderForAccount).andReturn().response
+            def cookie2 = response.getCookie("dev-ims-ihtsdo")
+
+        then:
+            response.status == HttpStatus.OK.value()
+            response.contentAsString.contains("login")
+            response.contentAsString.contains("joe")
+            cookie.getName() == "dev-ims-ihtsdo"
+            cookie.getValue() == "SomeToken"
+            cookie2 == null
     }
 }
